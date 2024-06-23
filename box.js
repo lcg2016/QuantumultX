8f30e38920d3ef3245baf89ea65d7ee071075e54
lcg2016 <33574272+lcg2016@users.noreply.github.com>
2024-06-23 09:47:39 +0800

    测试

--- box.js
+++ box.js
@@ -6,8 +6,8 @@
   "repo": "https://ooxx.be/js",
   "apps": [
     {
-      "id": "umer",
-      "name": "优麦医生",
+      "id": "ceshi",
+      "name": "测试",
       "keys": ["@UMER.CookieUM"],
       "settings": [
         {
@@ -25,14 +25,55 @@
       "icons": ["https://ooxx.be/js/icon/umer-trn.png", "https://ooxx.be/js/icon/umer.png"],
       "script": "https://ooxx.be/js/umer.js"
     },
-    {
-      "id": "jegotrip",
-      "name": "无忧行",
-      "keys": ["@无忧行.AsVow"],
-      "author": "@AsVow",
-      "repo": "https://ooxx.be/js",
-      "icons": ["https://ooxx.be/js/icon/jegotrip-trn.png", "https://ooxx.be/js/icon/jegotrip.png"],
-      "script": "https://ooxx.be/js/jegotrip.js"
-    }
+     {
+      "id": "cookie",
+      "name": "获取京东cookie",
+      "author": "@lcg",
+      "descs_html": ["用于自动抓取京东WSKEY"],
+      "icons": [
+        "https://raw.githubusercontent.com/FoKit/Scripts/main/images/jdjoy.png",
+        "https://raw.githubusercontent.com/FoKit/Scripts/main/images/jdjoy.png"
+      ],
+       "script": "https://raw.githubusercontent.com/FoKit/Scripts/main/scripts/weibo_sign.js",
+      "keys": [
+        "CK_AUTO_UPLOAD",
+        "CK_TG_BOT_TOKEN",
+        "CK_TG_USER_ID",
+        "COOKIE"
+      ],
+      "settings": [
+        {
+          "id": "WSKEY_AUTO_UPLOAD",
+          "name": "自动提交WSKEY",
+          "val": true,
+          "type": "boolean",
+          "desc": "启用后, 重写抓取的WSKEY会自动提交(上车)"
+        },
+        {
+          "id": "WSKEY_TG_BOT_TOKEN",
+          "name": "Telegram机器人Token",
+          "val": "",
+          "type": "text",
+          "desc": "填写 Telegram 机器人的 Token, 格式: 123456:abcdefgabcdefgabcdefg"
+        },
+        {
+          "id": "WSKEY_TG_USER_ID",
+          "name": "Telegram 会话ID",
+          "val": "",
+          "type": "text",
+          "desc": "填写 Telegram 会话ID: -1001701121614"
+        },
+        {
+          "id": "COOKIE",
+          "name": "京东COOKIE",
+          "val": "",
+          "type": "textarea",
+          "autoGrow": false,
+          "rows": 10,
+          "desc": "京东COOKIE"
+        }
+      ]
+    },
+   
   ]
 }
