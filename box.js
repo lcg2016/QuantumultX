4b4ffe4558e5f6499947afddf2ed854c3c9ccfc2
lcg2016 <33574272+lcg2016@users.noreply.github.com>
2024-06-23 09:38:59 +0800

    Create box.js
    BOXJS

---
+++ box.js
@@ -0,0 +1,38 @@
+{
+  "id": "asvow.app.sub",
+  "name": "LCG",
+  "author": "@LCG",
+  "icon": "https://avatars.githubusercontent.com/u/88471740",
+  "repo": "https://ooxx.be/js",
+  "apps": [
+    {
+      "id": "umer",
+      "name": "优麦医生",
+      "keys": ["@UMER.CookieUM"],
+      "settings": [
+        {
+          "id": "@UMER.CookieUM",
+          "name": "UMER Token",
+          "val": null,
+          "type": "textarea",
+          "autoGrow": true,
+          "rows": 1,
+          "desc": ""
+        }
+      ],
+      "author": "@LCG",
+      "repo": "https://ooxx.be/js",
+      "icons": ["https://ooxx.be/js/icon/umer-trn.png", "https://ooxx.be/js/icon/umer.png"],
+      "script": "https://ooxx.be/js/umer.js"
+    },
+    {
+      "id": "jegotrip",
+      "name": "无忧行",
+      "keys": ["@无忧行.AsVow"],
+      "author": "@AsVow",
+      "repo": "https://ooxx.be/js",
+      "icons": ["https://ooxx.be/js/icon/jegotrip-trn.png", "https://ooxx.be/js/icon/jegotrip.png"],
+      "script": "https://ooxx.be/js/jegotrip.js"
+    }
+  ]
+}
